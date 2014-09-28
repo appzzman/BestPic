fcf4a7a328fe88ec7643fadf46daa5d294303f79
sadmin <jchudzynski@uwf.edu>
2014-09-28 16:04:07 -0500

    Counts number of photos and likes
    detected bug, done function is called twice when the count is zero. One
    of the if statements is wrong.

--- BestPic/ViewController.swift
+++ BestPic/ViewController.swift
@@ -9,46 +9,56 @@
 
 import UIKit
 
-class Photo
+class Photo : Printable
 {
-    var id:Int = -1
+    var id:Int = 0
     var image_url:String = ""
-    var likes_count:String = ""
+    var thumb_url:String = ""
+    var likes_count:Int = 0
+    var description: String { get { return "Photo: id = \(id) image_url = \(image_url) likes = \(likes_count)" } }
     
 }
+
+
+class UserStats : Printable {
+    
+    var userPhotos:Array<Photo> = Array()
+    var likeCount:Int = 0
+    var photoCount:Int  = 0
+    
+    var description: String { get { return "User Stats: like count = \(likeCount)  photoCount = \(photoCount)" } }
+    
+    
+}
+
     
 
 class ViewController: UIViewController, FBLoginViewDelegate {
     
-    
+    var albumsToProcess:Dictionary<String,AnyObject> = Dictionary()
+    var globalRawPhotoDictionary:Dictionary<String, AnyObject> = Dictionary()
+    var globalRawPhotoArray:Array<AnyObject> = Array()
+    var globalsortedPhotoArray:Array<Photo> = Array()
+    var userStats = UserStats()
+
+    //var operationqueue: NSOperationQueue = NSOperationQueue()
     
     @IBOutlet var fbLoginView : FBLoginView!
-    var lock:NSConditionLock = NSConditionLock();
+   // var lock:NSConditionLock = NSConditionLock();
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-         self.fbLoginView.delegate = self
+        self.fbLoginView.delegate = self
         self.fbLoginView.readPermissions = ["public_profile", "email", "user_friends","user_photos"]
         
-       
-       // self.getPermissions()
-        var array:Array<AnyObject> =  Array();
-        //fields=id,name,picture"
-       self.getPictures(array,url: "/me/photos?fields=id,likes&limit=100")
-        
-        //self.getAlbums();
-        
-        
-        
-//        
-//        self.getPictures(array, finished:(photoArray) in
-//            println("Done:!!!!")
-//            println(photoArray)
-//        })
-    
+        var date:NSDate = NSDate()
+        println(date)
+        self.getAlbums(globalRawPhotoArray);
     }
     
+    
+    ///Converts absolute url to iOS friendly url
     func getRealURL(url:String)->String{
         var array:Array<String>  = url.componentsSeparatedByString("/")
         
@@ -95,8 +105,12 @@ class ViewController: UIViewController, FBLoginViewDelegate {
    
     func getPermissions(){
         FBRequestConnection.startWithGraphPath("/me/permissions", parameters: nil, HTTPMethod: "GET", completionHandler: { (connection:FBRequestConnection!,  result:AnyObject!, error:AnyObject!) -> Void in
-            println(error);
-            println(result);
+            if(error != nil){
+                println(error);
+            }
+            else{
+                println(result);
+            }
             
         })
     }
@@ -104,105 +118,244 @@ class ViewController: UIViewController, FBLoginViewDelegate {
     
     func done(photoArray:Array<AnyObject>){
        
-        println("Done");
-        println(photoArray.count);
+        self.globalRawPhotoArray += photoArray
+        
+        if self.albumsToProcess.count == 0 {
+            self.getLikes(self.globalRawPhotoArray);
+        }
+        else{
+           // println(self.albumsToProcess);
+        }
+    }
+    
+    func getLikes(photoArray:Array<AnyObject>)
+    {
 
-        //get albums
+        //Preparea global dictionary to process
+        for (var i = 0; i < photoArray.count; i++){
+            let photo: AnyObject = photoArray[i]
+            if let pid: String = photo["id"] as? String
+            {
+                self.globalRawPhotoDictionary[pid] = pid
+            }
+ 
+        }
         
+        for (var i = 0; i < photoArray.count; i++){
+            let photo: AnyObject = photoArray[i]
+            self.getLikesForPhoto(photo)
         
+        }
+    }
+    
+    
+    func getMoreLikes(photo:Photo, url:String){
+ 
+            FBRequestConnection.startWithGraphPath(url, parameters: nil, HTTPMethod: "GET", completionHandler: { (connection:FBRequestConnection!,  result:AnyObject!, error:AnyObject!) -> Void in
+                
+                if(error == nil){
+                    
+                    var photosDict:Dictionary = result as Dictionary<String, AnyObject>
+                    
+                    // var ph:AnyObject = photos["data"]!;
+                    if let ph = photosDict["data"] as AnyObject? as? Array <AnyObject>
+                    {
+                        photo.likes_count += ph.count;
+                        
+                    }
+                    
+                    
+                    if let paging = photosDict["paging"] as AnyObject? as? Dictionary <String, AnyObject>{
+                        //drill down to get a link
+                        if let next = paging["next"] as AnyObject? as? String
+                        {
+                            
+                            self.getMoreLikes(photo, url: self.getRealURL(next))
+                        }
+                        else{
+                            self.insertSort(photo)
+                        }
+                    }
+                    else{
+                        self.insertSort(photo)
+
+                    }
+                }
+                else{
+                     self.insertSort(photo)
+                    println("error \(error)")
+                    
+                    }
+                }
+                
+            )
         
     }
     
-    func getAlbums()->Void{
-        FBRequestConnection.startWithGraphPath("/me/albums", parameters: nil, HTTPMethod: "GET", completionHandler: { (connection:FBRequestConnection!,  result:AnyObject!, error:AnyObject!) -> Void in
+    
+    func getLikesForPhoto(rawPhoto:AnyObject){
+       // println("Raw Photo_______________________________________________")
+     //   println(rawPhoto);
+        var photo:Photo = Photo()
+        
+        if let pid: String? = rawPhoto["id"] as? String
+        {
+            photo.id = pid!.toInt()!
+      
+        }
+        if let imgurl: String = rawPhoto["picture"] as? String
+        {
+            photo.image_url = imgurl
+        }
+
+        
+        if let likesDictionary : Dictionary<String, AnyObject> = rawPhoto["likes"] as? Dictionary<String, AnyObject>{
+            let dataArray:Array<AnyObject> = likesDictionary["data"] as AnyObject? as Array<AnyObject>
+                photo.likes_count = dataArray.count
+
+            if let paging:Dictionary<String, AnyObject> = likesDictionary["paging"] as AnyObject! as? Dictionary <String, AnyObject>
+            {
+                if let next = paging["next"] as AnyObject? as? String
+                {
+                    var url = self.getRealURL(next);
+                    self.getMoreLikes(photo, url: url)
+                }
+                else{//it doesn't have next which means that we need to just update
+                    self.insertSort(photo)
+                }
+            }
+            else{
+                self.insertSort(photo)
+            }
+        }
+        else{
+              self.insertSort(photo)
+        }
+    }
+    
+    ///Inserts photo element and sorts the array
+    func insertSort(photo:Photo){
+        
+        self.globalsortedPhotoArray.append(photo)
+        self.globalRawPhotoDictionary.removeValueForKey(String(photo.id))
+        self.userStats.likeCount += photo.likes_count
+        
+        println(self.globalRawPhotoDictionary.count)
+        
+        if self.globalRawPhotoDictionary.count == 0 {
+            //wow we are done.
+            println("done with inserting photos")
+            self.globalsortedPhotoArray.sort({$0.likes_count>$1.likes_count})
+            self.userStats.userPhotos = self.globalsortedPhotoArray
             
-            // println(result);
+            println(self.userStats)
+            var date:NSDate = NSDate()
+            println(date)
             
+        }
+    }
+    
+    
+    
+    func getAlbums(photoArray:Array<AnyObject>)->Void{
+
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), { () -> Void in
+
+            println("Background Operation")
+            
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                //done with one operation
+                self.view.backgroundColor = UIColor.blackColor()
+                
+            })
+        })
+        
+            FBRequestConnection.startWithGraphPath("/me/albums?fields=id,photos.fields(id,picture,thumb,likes),name", parameters: nil, HTTPMethod: "GET", completionHandler: { (connection:FBRequestConnection!,  result:AnyObject!, error:AnyObject!) -> Void in
+                
+                print(error);
+                
                 if(error == nil){
                     var albumsDict:Dictionary = result as Dictionary<String, AnyObject>
-                    println(result);
                     
-            }
-         }
-        )
-    }
+                    if let photoData = albumsDict["data"] as AnyObject? as? Array<AnyObject> {
+                        for (index, element) in enumerate(photoData) {
+                            let name =  element["name"] as String;
+                            //now each album photos
+                            if let photosData = element["photos"] as AnyObject? as? Dictionary <String, AnyObject>{
+                                if let photosArray = photosData["data"] as AnyObject? as? Array<AnyObject>{
+                                    if let paging = photosData["paging"] as AnyObject? as? Dictionary <String, AnyObject>{
+                                        if let next = paging["next"] as AnyObject? as? String
+                                        {
+                                            self.albumsToProcess[name] = name
+                                            //found more elements to download
+                                            var url = self.getRealURL(next);
+                                            self.getPictures(photosArray, url: url, name:name)
+                                        }
+                                        else{
+                                        }
+                                    }
+                                    else{
+                                    }
+                                    
+                                    self.done(photosArray);
+                                    
+                                }
+                            }
+                        }
+                    }
+                }
+            })
+      }
     
-    func getPictures(photoArray:Array<AnyObject>, url:String){
+    func getPictures(photoArray:Array<AnyObject>, url:String, name:String ){
         var photos:Array <AnyObject> = Array();
-        photos = photos + photoArray;
-        
+        photos += photoArray;
         
         FBRequestConnection.startWithGraphPath(url, parameters: nil, HTTPMethod: "GET", completionHandler: { (connection:FBRequestConnection!,  result:AnyObject!, error:AnyObject!) -> Void in
             
-           // println(result);
-            
             if(error == nil){
-              
+                
                 var photosDict:Dictionary = result as Dictionary<String, AnyObject>
                
                // var ph:AnyObject = photos["data"]!;
                 if let ph = photosDict["data"] as AnyObject? as? Array <AnyObject>
                 {
                     photos =  photos + ph;
-                    println(photos.count);
-                    
-                  //  let likes: ph["likes"] as AnyObject? as Dictionary<String, AnyObject>
-                    
-                    
+             
                     
                 }
                 
-                println("START____________________________________________________________________________");
-                
-                println(url)
-                println(photosDict)
-                
-                
-                println("E____________________________________________________________________________");
+       
                 if let paging = photosDict["paging"] as AnyObject? as? Dictionary <String, AnyObject>{
                     //drill down to get a link
                     if let next = paging["next"] as AnyObject? as? String
                     {
                         
-                        println("Next Element Is: ")
+                    //    println("Next Element Is: ")
                         var url = self.getRealURL(next);
-                        println(url)
+                    //    println(url)
                         
                         
-                        self.getPictures(photos, url: url)
+                        self.getPictures(photos, url: url, name:name)
                     }
                     else{
+                        self.albumsToProcess.removeValueForKey(name);
                         self.done(photos)
                     }
                 }
                 else{
-                        self.done(photos)
+                    self.albumsToProcess.removeValueForKey(name);
+                    self.done(photos)
                 }
             }
             else{
                 println("Error")
                 println(error);
-
+                self.albumsToProcess.removeValueForKey(name);
                 self.done(photos);
             }
-
-        
-        })
-        
-        /*
-        /* make the API call */
-        [FBRequestConnection startWithGraphPath:@"/me"
-        parameters:nil
-        HTTPMethod:@"GET"
-        completionHandler:^(
-        FBRequestConnection *connection,
-        id result,
-        NSError *error
-        ) {
-            /* handle the result */
-        }];
-    */
-}
-    
+        }
+      )
+    }
 }
 
